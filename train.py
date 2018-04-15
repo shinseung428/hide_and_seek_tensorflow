e78a3b120b81a6b9b9599a18f0f1dd63cb44d992
shinseung428 <shinseung428@gmail.com>
2018-04-15 20:08:49 +0900

    update

--- train.py
+++ train.py
@@ -88,7 +88,7 @@ def train(args, sess, model):
 
             tr_batch = []
 
-            tr_batch = [load_image(path, args, is_training=False) for path in tr_img_batch]
+            tr_batch = [load_image(path, args, is_training=True) for path in tr_img_batch]
             val_batch = [load_image(path, args, is_training=False) for path in val_img_batch]
             tr_batch = np.asarray(tr_batch)
             val_batch = np.asarray(val_batch)
