91f00f00ed1925915c7e6bb4bf6cc5489a9d9afc
shinseung428 <shinseung428@gmail.com>
2018-04-15 22:12:19 +0900

    update

--- ops.py
+++ ops.py
@@ -83,7 +83,7 @@ def load_image(path, args, is_training=True):
 		image = image / 255.0
 		# use 13x13 grid
 		# grid = np.random.randint(10, 15)
-		grid = np.random.choice([10,13,20], 1)[0]
+		grid = np.random.choice([8,13,16], 1)[0]
 		breaks = args.input_width // grid + 1
 		for x in range(breaks):
 			for y in range(breaks):
