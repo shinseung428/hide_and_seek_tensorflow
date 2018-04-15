adee1c3a58ec73f09f392b3d52aacf36c20fb026
shinseung428 <shinseung428@gmail.com>
2018-04-15 19:50:00 +0900

    update

--- ops.py
+++ ops.py
@@ -70,7 +70,7 @@ def load_image(path, args, is_training=True):
 		if prob > 0.5 and aug == 1:
 			image = np.flip(image, 1)
 		elif prob > 0.5 and aug == 2:
-			image = scipy.ndimage.interpolation.rotate(image, np.random.randint(-20,20))
+			image = scipy.ndimage.interpolation.rotate(image, np.random.randint(-15,15))
 			image = scipy.misc.imresize(image, (args.input_width, args.input_height))
 		elif prob > 0.5 and aug == 3:
 			image = scipy.ndimage.interpolation.zoom(image, (1.5,1.5,1.0))
@@ -79,14 +79,13 @@ def load_image(path, args, is_training=True):
 		image = image / 255.0
 		# use 13x13 grid
 		# grid = np.random.randint(10, 15)
-		grid = np.random.choice([8,13,16], 1)[0]
-
-		breaks = args.input_width // grid + 1
-		for x in range(breaks):
-			for y in range(breaks):
-				prob = np.random.rand()
-				if prob >= 0.5:
-					image[x*grid:x*grid+grid,y*grid:y*grid+grid,:] = mean
+		# grid = np.random.choice([8,13,16], 1)[0]
+		# breaks = args.input_width // grid + 1
+		# for x in range(breaks):
+		# 	for y in range(breaks):
+		# 		prob = np.random.rand()
+		# 		if prob >= 0.5:
+		# 			image[x*grid:x*grid+grid,y*grid:y*grid+grid,:] = mean
 	else:
 		image = image / 255.0
 
